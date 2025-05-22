22fd1b8ad95649b0a3de830c425e5f892543bd31
Jax Lehmann <jaxlehmann@gmail.com>
2025-05-22 16:02:12 -0400

    Add files via upload
    leetcode 203, Remove Linked List Elements. Done by moving pointers with a dummy node. Struggled a lot with this one, need to revisit it and practice more with Linked Lists. Needed lots of assistance, despite having the right logic. Coding it was difficult; mainly with trying to keep track of the pointers.

---
+++ RemoveLinkedElements.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class RemoveLinkedElements {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = head;
+        dummy.next = head;
+        ListNode prev = dummy;
+        if (head == null) {
+            return null;
+        }
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            }
+            else {
+                prev = curr;
+            }
+            curr = curr.next;
+
+        } return dummy.next;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
