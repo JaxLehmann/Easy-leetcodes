ca8ba94bf0f133aa42227cbfe85ceca1ab37fbcc
Jax Lehmann <jaxlehmann@gmail.com>
2025-05-23 15:10:45 -0400

    Add files via upload

---
+++ ReverseLinkedList.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+/* 
+given LL, return reverse list
+can loop through this iteratively
+set 3 pointers
+previous pointer to null
+current pointer to head
+next pointer to the node after current
+each loop, shift pointer to previous
+return previous
+*/
