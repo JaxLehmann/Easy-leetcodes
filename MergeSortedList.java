49c75dae5b8784b99d3b1cbfb56b857060aaf939
Jax Lehmann <jaxlehmann@gmail.com>
2025-05-27 01:12:12 -0400

    Add files via upload
    leetcode problem #21. Solved by using a dummy node and comparison loop.
    Sidenote: Also noticed that this could be done by merging the problems into a B-Tree, then using an Inorder Traversal to return it sorted in increasing order. Was not able to write that code off the top of my head though, but was a cool nod to my DSA class.

---
+++ MergeSortedList.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class MergeSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+        if (list1 != null) {
+            tail.next = list1;
+        }
+        else {
+            tail.next = list2;
+        }
+        return dummy.next;
+    }
+}
+/*
+given heads of two sorted lists l1 l2
+merge two lists into one sorted list
+the list should be made by splciing together nodes of first two lists
+return head of merged ll ======
+sorted in increasing
+Compare list1 val to list 2
+Dummy node to move pointer
+*/
